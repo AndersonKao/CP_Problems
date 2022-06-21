5e3f1dc488ae88243c4d96808e61c2d871c96394
TunchinKao <tunchinkao@gmail.com>
2022-06-21 18:26:40 +0800

    starting WF2005pA

---
+++ WF/WF2005/2005pA.cpp
@@ -0,0 +1,226 @@
+#include <bits/stdc++.h>
+using namespace std;
+const double eps = 1e-6;
+inline int fcmp(const double &a, const double &b){
+    if(fabs(a - b) < eps)
+        return 0;
+    return ((a - b > 0.0) * 2) - 1;
+}
+
+template <typename T>
+struct Point
+{
+    T x, y;
+    Point() : x(0), y(0) {}
+    Point(const T x, const T y) : x(x), y(y) {}
+    template <class F>
+    explicit operator Point<F>() const
+    {
+        return Point<F>((F)x, (F)y);
+    }
+
+    Point operator+(const Point b) const
+    {
+        return Point(x + b.x, y + b.y);
+    }
+    Point operator-(const Point b) const
+    {
+        return Point(x - b.x, y - b.y);
+    }
+    template <class F>
+    Point<F> operator*(const F fac)
+    {
+        return Point<F>(x * fac, y * fac);
+    }
+    template <class F>
+    Point<F> operator/(const F fac)
+    {
+        return Point<F>(x / fac, y / fac);
+    }
+
+    T operator&(const Point b) const { return x * b.x + y * b.y; }
+    // dot operator
+    T operator^(const Point b) const { return x * b.y - y * b.x; }
+    // cross operator
+    bool operator==(const Point b) const
+    {
+        return x == b.x and y == b.y;
+    }
+    bool operator<(const Point b) const
+    {
+        return x == b.x ? y < b.y : x < b.x;
+    } // 字 典 序
+
+    Point operator-() const { return Point(-x, -y); }
+    T norm() const { return *this & *this; }    // 歐 式 長 度 平 方
+    Point prep() const { return Point(-y, x); } // 左 旋 直 角 法 向 量
+    template <class F>
+    friend istream &operator>>(istream &is, Point<F> &pt);
+    template<class F>
+    friend ostream &operator<<(ostream &os, const Point<F> &pt);
+};
+template <class F>
+ostream &operator<<(ostream &os, const Point<F> &pt)
+{
+    return os << pt.x << " " << pt.y;
+}
+template <class F>
+istream &operator>>(istream &is, Point<F> &pt)
+{
+    return is >> pt.x >> pt.y;
+}
+template <class F>
+bool collinearity(const Point<F>& p1, const Point<F>& p2, const Point<F>& p3){
+//    return (p1 - p3) ^ (p2 - p3) == 0;
+    return fcmp((p1 - p3) ^ (p2 - p3), 0.0) == 0;
+}
+// check co-line first. properly
+template<class F>
+inline bool btw(const Point<F>& p1, const Point<F>& p2, const Point<F>& p3){
+    return fcmp((p1 - p3) & (p2 - p3), 0.0) <= 0;
+}
+
+// is p3 on (p1, p2)?
+template<class F>
+inline bool pointOnSegment(const Point<F>& p1, const Point<F>& p2, const Point<F>& p3){
+    return collinearity(p1, p2, p3) && btw(p1, p2, p3);
+}
+
+template <typename T, typename Real = double>
+struct Line
+{
+    Point<T> st, ed;
+    Point<T> vec() const { return ed - st; }
+    T ori(const Point<T> p) const { return (ed - st) ^ (p - st); }
+    int orint(const Point<T> p) const{
+        T a = this->ori(p);
+        return (fcmp(a, 0.0), 0); // 1 on posi-side // -1 nega-side
+        // a little bit useless?
+    }
+    Line(const Point<T> x, const Point<T> y) : st(x), ed(y) {}
+    template <class F>
+    operator Line<F>() const
+    {
+        return Line<F>((Point<F>)st, (Point<F>)ed);
+    }
+
+    // sort by arg, the left is smaller for parallel lines
+    bool operator<(Line B) const
+    {
+        Point<T> a = vec(), b = B.vec();
+        auto sgn = [](const Point<T> t) { return (t.y == 0 ? t.x : t.y) < 0; };
+        if (sgn(a) != sgn(b))
+            return sgn(a) < sgn(b);
+        if (abs(a ^ b) == 0)
+            return B.ori(st) > 0;
+        return (a ^ b) > 0;
+    }
+
+    // Regard a line as a function
+    template <typename F>
+    Point<F> operator()(const F x) const // A + AB * x = the point position.
+    {
+        return Point<F>(st) + vec() * x;
+    }
+
+    bool isSegProperIntersection(const Line l) const
+    {
+        return l.ori(st) * l.ori(ed) < 0 and ori(l.st) * ori(l.ed) < 0;
+    }
+    bool isSegIntersection(const Line l)const{
+        Line<Real> h = *this;
+        // hst = 1, hed = 2, lst = 3, led = 4
+        double hlst = h.ori(l.st);
+        double hled = h.ori(l.ed);
+        double lhst = l.ori(h.st);
+        double lhed = l.ori(h.ed);
+        if(fcmp(hlst, 0.0) == 0 && fcmp(hled,0.0) == 0)
+            return h.isPointOnSeg(l.st) || h.isPointOnSeg(l.ed) || l.isPointOnSeg(h.st) || l.isPointOnSeg(h.ed);
+
+        return fcmp(hlst * hled, 0.0) <= 0 && fcmp(lhst * lhed, 0.0) <= 0;
+    }
+    
+    bool isPointOnSegProperly(const Point<T> p) const
+    {
+        return fcmp(ori(p), 0.0) == 0 and fcmp(((st - p) & (ed - p)), 0.0) < 0;
+    }
+    bool isPointOnSeg(const Point<T>p) const{
+        return fcmp(ori(p), 0.0) == 0 and fcmp((st - p) & (ed - p), 0.0) <= 0;
+    }
+    Real disP2Line(const Point<T> p) const
+    {
+        return Line<double>(projection(p), Point<double>(p)).vec().norm();
+    }
+
+    // notice if you should check Segment intersect or not;
+    // be careful divided by 0
+    Point<Real> getIntersection(Line<Real> l)
+    {
+        Line<Real> h = *this;
+//        return l(((l.st - h.st)^ h.vec()) / (h.vec() ^ l.vec())); // use operator()
+        Real hlst = -h.ori(l.st);
+        Real hled = h.ori(l.ed);
+        return ((l.st * hled) + (l.ed * hlst)) / (hlst + hled);
+
+        // 需要確認+-號的合理性
+        // Area of triangle(l.st, h.st, h.ed) divided by Area of Quadrilateral(h.st, l.st, h.ed, l.ed)
+    }
+
+    Point<Real> projection(const Point<T> p) const
+    {
+        return operator()(((p - st) & vec()) / (Real)(vec().norm()));
+    }
+    template <class F>
+    friend ostream &operator<<(ostream &os, const Line<F> &l);
+};
+template <class F>
+ostream &operator<<(ostream &os, const Line<F> &l)
+{
+    return os << "(" << l.st.x << ", " << l.st.y << ") to (" << l.ed.x << ", " << l.ed.y << ")";
+}
+template <class F>
+using Polygon = vector<Point<F>>;
+
+#define REP(i, n) for (int i = 0; i < (int)(n); i++)
+#define eb emplace_back
+#define al(x) x.begin(), x.end()
+#define F first
+#define S second
+template <typename T>
+using vec = vector<T>;
+using LineB = pair<Line<double>, bool>;
+
+int main(){
+
+    int n, m;
+    while ( cin >> n >> m ){
+        if(n == 0 && m == 0)
+            break;
+        vec<pair<Line<double>, bool>> puzzle, image; // vertical = true
+        REP(i, n){
+            Point<double> st, ed;
+            cin >> st >> ed;
+            if(st.x == ed.x)
+                puzzle.eb(st, ed, true);
+            else
+                puzzle.eb(st, ed, false);
+            
+        }
+        REP(i, m){
+            Point<double> st, ed;
+            cin >> st >> ed;
+            if(st.x == ed.x)
+                image.eb(st, ed, true);
+            else
+                image.eb(st, ed, false);
+        }
+        sort(al(puzzle));
+        sort(al(image));
+        for_each(al(puzzle), [](LineB &a)
+                 { cout << "Line " << a.F << " is " << (a.S ? "vertical " : "horizontal") << endl; });
+        for_each(al(image), [](LineB &a)
+                 { cout << "Line " << a.F << " is " << (a.S ? "vertical " : "horizontal") << endl; });
+
+    }
+        return 0;
+}
