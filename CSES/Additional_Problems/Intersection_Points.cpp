99e11f94dc48ea5ffc83b72772d9c8072b6480cc
TunchinKao <tunchinkao@gmail.com>
2022-08-20 12:19:06 +0800

    CSES Intersection_Points

--- CSES/Additional_Problems/Intersection_Points.cpp
+++ CSES/Additional_Problems/Intersection_Points.cpp
@@ -1,3 +1,4 @@
+// once AC 2022-08-20 07:17:08
 #include <bits/stdc++.h>
 using namespace std;
 #define al(x) x.begin(), x.end()
@@ -18,15 +19,22 @@ struct event{
 	event(){}
 	event(int t, int x, int y):type(t), x(x), y(y){};
 	event(int t, int x, int y, int len):type(t), x(x), y(y), len(len){};
+	int operator<(const event& e){
+		return x < e.x;
+	}
 	friend ostream& operator<<(ostream& os, const event& e);
 };
 ostream& operator<<(ostream& os, const event& e){
-	return (os << e.type << ", " << e.r << ", " << e.c << ", " << e.len);
+	return (os << e.type << ", " << e.x << ", " << e.y << ", " << e.len);
 }
 struct segT{
 	vector<int> seq;
 //	vector<int> tag;
 	int N;
+	segT(){};
+	segT(int n){
+		this->Build(n);
+	}
 	void Build(int n){
 		seq.clear();
 		seq.resize(4*n, 0);
@@ -45,28 +53,6 @@ struct segT{
 		}
 		int M = (L+R)>>1;
 		return rangeS(L, M, l, r, LC(idx)) + rangeS(M+1, R, l, r, RC(idx));
-	}
-	int Query(int l, int r){
-		return query(0, N-1, l, r,0);
-	}
-	int query(int L ,int R, int l, int r, int idx){
-		if(L > r || R < l)
-			return -1;
-#ifdef DsegT
-		cout << "query " << L << ", " << R << endl;
-#endif
-		if(L == R)
-			return L;
-		int M = (L+R)>>1;
-		int li = LC(idx), ri = RC(idx);
-		int ans = -1;
-		if(seq[li] > 0){
-			ans = query(L, M, l, r, li);
-		}
-		if(ans == -1 && seq[ri] >= 0){
-			ans = query(M+1, R, l, r, ri);
-		}
-		return ans;
 	}
 	void Modify(int pos, int val){
 		modify(0, N-1, 0, pos, val); 
@@ -96,91 +82,39 @@ struct segT{
 		print(M+1, R, RC(idx));
 	}
 };
+const int offset = 1000000;
 int main(){
 	int n;
 	cin >> n;
-	vector<event>;
+	vector<event> eQ;
+	int x1, y1, x2, y2;
 	for(int i = 0; i < n; i++){
 		cin >> x1 >> y1 >> x2 >> y2;
 		
-		if(y1 == y2){ // -------
-			
+		if(y1 != y2){ // -------
+			eQ.eb(2, x1, y1 + offset, y2 - y1);						
+		}
+		else{
+			eQ.eb(0, x1, y1 + offset);
+			eQ.eb(1, x2, y2 + offset);
 		}
 	}
 
-				Es[0].eb(0, e.st.r, e.st.c, -1);	
-				Es[0].eb(1, e.ed.r, e.ed.c, -1);	
-			}
-			else{
-				Es[1].eb(2, e.st.r + 1, e.st.c, e.ed.r - e.st.r - 2);
-			}
+	sort(al(eQ));
+	segT mT(2000002);
+	ll ans = 0;
+	for(event& e: eQ){
+//		cout << e << endl;
+		if(e.type == 0){
+			mT.Modify(e.y, 1);
 		}	
-		for(auto&e: Detector){ // redline
-			if(e.st.r == e.ed.r){ // -------
-				Es[1].eb(0, e.st.r, e.st.c, -1);	
-				Es[1].eb(1, e.ed.r, e.ed.c, -1);	
-			}
-			else{
-				Es[0].eb(2, e.st.r + 1, e.st.c, e.ed.r - e.st.r - 2);
-			}
-		}	
-		auto ecmp = [](const event& lhs, const event&rhs){
-			return (lhs.c == rhs.c ? (lhs.type < rhs.type) : (lhs.c < rhs.c));
-		};	
-		for(int i = 0; i < 2; i++)
-			sort(al(Es[i]), ecmp);
-		segT mT;
-		int ans = 0;
-		point apt(2*R, 2*C);
-		for(int j = 0; j < 2; j++){
-//			cout << "round " << j << "------------------------\n";
-			mT.Build(R+2);
-			for(int i = 0; i < Es[j].size(); i++){
-				event& e = Es[j][i];
-//				cout << "event: " << e << endl;
-				if(e.type == 0){
-					mT.Modify(e.r, 1);
-				}
-				else if(e.type == 1){
-					mT.Modify(e.r, -1);
-				}
-				else {
-					int itsc = mT.RangeS(e.r, e.r + e.len);
-					ans += itsc;
-					if(itsc > 0){
-//						cout << "get inter \n";
-						int rpos = mT.Query(e.r, e.r+e.len);	
-//						debug(rpos);
-						if(rpos < apt.r){
-							apt.r = rpos;
-							apt.c = e.c;
-#ifdef DsegT
-							cout << "0: " << apt << endl;
-#endif
-						}
-						else if(rpos == apt.r && e.c < apt.c){
-							apt.c = e.c;	
-#ifdef DsegT
-							cout << "1: " << apt << endl;
-#endif
-						}
-					}
-				}
-#ifdef DsegT
-				mT.Print();
-#endif
-			}
+		else if(e.type == 1){
+			mT.Modify(e.y, -1);
 		}
-		cout << "Case " << caseN++ << ": ";
-		if(ans == 0){
-			cout << "impossible";
+		else {
+			ans += mT.RangeS(e.y, e.y + e.len);
 		}
-		else{
-			cout << ans << " " << apt.r << " " << apt.c;
-		}
-		cout << endl;
-
-	}	
-
+	}
+	cout << ans << endl;
 	return 0;
 }
