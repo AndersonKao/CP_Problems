e4e645cf9ab6d75f611470f2d59d8842eb39ec2e
TunchinKao <tunchinkao@gmail.com>
2022-08-31 20:54:22 +0800

    renew tmp

--- tmp.cpp
+++ tmp.cpp
@@ -4,6 +4,7 @@ using namespace std;
 #define REP1(i, n) for(int i = 1; i <= (int)n; i++)
 #define debug(x) cout << #x << ": " << x << endl;
 #define depii(x) cout << "(" << x.first << ", " << x.second << ")";
+#define eb emplace_back
 #define F first
 #define S second
 #define al(x) x.begin(), x.end()
