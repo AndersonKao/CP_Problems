8c1102626a61c0d8620a90a0c2bb5e1b32d52d6f
Anderson Kao <tunchinkao@gmail.com>
2024-07-18 11:51:42 +0800

    Attacking Mail Delivery

---
+++ CSES/Graph_Algorithms/Mail_Delivery.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>;
+using namespace __gnu_pbds;
+template<typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, treeorder_statistics_node_update>;
+*/
+#define REP0(i, n) for (int i = 0; i < n; i++)
+#define REP(i, a, b) for(int i = a; i <= b; i++)
+#define debug(x) cout << #x << ": " << x << endl;
+#define depii(x) cout << "(" << x.first << ", " << x.second << ")";
+#define devec(x) for(auto v: x) cout << v << endl;
+#define eb emplace_back
+#define F first
+#define S second
+#define al(x) x.begin(), x.end()
+#define mp make_pair
+#define yccc cin.tie(0), ios_base::sync_with_stdio(false)
+#define ln '\n'
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+template<typename T>
+using p = pair<T, T>;
+
+template<typename T>
+using vec = vector<T>;
+
+vec<int> G[100001];
+
+vec<int> ans[100001];
+
+void dfs(int u, int pa, int st){
+	if(pa != -1 && u == st){
+		G[
+		return;
+	}
+}
+
+int main(){
+	yccc;		
+	int n, m;
+	cin >> n >> m;
+	int u, v;
+	for(int i = 0; i < m; i++){
+		cin >> u >> v;
+		G[u].eb(v);
+		Gdafadsjfoijsad'f 
+			
+		oojjfjsdfjkadjjfjv].eb(u);
+		deg[u]++;
+		deg[v]++;
+	}
+
+	for(int u = 1; u <= n; u++){
+		if(deg[u] & 1){
+			cout << "IMPOSSIBLE\n";
+			return 0;
+		}
+	}
+	int edgenum = m;
+	dfs(1, -1, 1);
+	while(edgenum >= 1){
+		
+	}
+	return 0;
+}
+
