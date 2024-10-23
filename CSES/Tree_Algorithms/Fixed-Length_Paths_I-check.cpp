5374aa1d246b9c88429400270bea0832bc08afe1
TunchinKao <tunchinkao@gmail.com>
2024-10-23 15:49:07 +0800

    add checker program

---
+++ CSES/Tree_Algorithms/Fixed-Length_Paths_I-check.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>;
+using namespace __gnu_pbds;
+template<typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, treeorder_statistics_node_update>;
+*/
+#define REP0(i, n) for (int i = 0; i < n; i++)
+#define REP(i, a, b) for(int i = a; i <= b; i++)
+#define debug(x) cout << #x << ": " << x << endl;
+#define depii(x) cout << "(" << x.first << ", " << x.second << ")";
+#define devec(x) for(auto v: x) cout << v << endl;
+#define eb emplace_back
+#define F first
+#define S second
+#define al(x) x.begin(), x.end()
+#define mp make_pair
+#define yccc cin.tie(0), ios_base::sync_with_stdio(false)
+#define ln '\n'
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+template<typename T>
+using p = pair<T, T>;
+
+template<typename T>
+using vec = vector<T>;
+const int maxn = 200000;
+bool been_centroid[maxn];
+int depth[maxn], sz[maxn];
+vector<vector<int>> G;
+ll ans = 0;
+int n, k;
+
+void dfs(int u, int pa){
+	if(pa == -1){
+		depth[u] = 0;
+	}
+	else{
+		depth[u] = depth[pa]+1;
+		if(depth[u] == k){
+			ans++;
+		}
+	}
+	for(int v: G[u]){
+		if(v == pa) continue;
+		dfs(v, u);
+	}
+}
+
+int main(){
+	yccc;
+	cin >> n >> k;
+	G.resize(n);
+	for(int i = 0; i < n-1; i++){
+		int a, b;
+		cin >> a >> b;
+		a--,b--;
+		G[a].eb(b);
+		G[b].eb(a);
+	}
+	for(int u = 0; u < n; u++){
+		dfs(u, -1);
+	}
+	cout << ans/2 << endl;
+	return 0;
+}
