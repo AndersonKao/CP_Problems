8ecb32ed21c8184533e77afdace94c783affb011
TunchinKao <tunchinkao@gmail.com>
2024-10-25 18:59:28 +0800

    Solve Centroid Decomposition

--- CSES/Tree_Algorithms/Fixed-Length_Paths_I.cpp
+++ CSES/Tree_Algorithms/Fixed-Length_Paths_I.cpp
@@ -1,4 +1,7 @@
 #include <bits/stdc++.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+
 using namespace std;
 /*
 #include <ext/pb_ds/assoc_container.hpp>
@@ -28,88 +31,107 @@ using p = pair<T, T>;
 template<typename T>
 using vec = vector<T>;
 const int maxn = 200000;
-bool been_centroid[maxn];
-int depth[maxn], sz[maxn];
-vector<vector<int>> G;
-int cnt[maxn + 1];
+bool been_centroid[maxn+5];
+int depth[maxn+5], sz[maxn+5];
+vector<list<int>> G;
+int cnt[maxn + 5];
 ll ans = 0;
 int n, k;
 
-int tree_centroid(int u, int pa, int tree_size)
+void print_tree(int u, int pa = -1){
+	cout << u << " ";
+
+	for(int v: G[u]){
+		if(v == pa) continue;
+		if(been_centroid[v]){
+			continue;
+		}	
+		print_tree(v, u);
+	}
+}
+int gloc;
+
+void tree_centroid(int u, int pa, int tree_size, int d =1)
 {
+//	debug(d);
+//	debug(u);
 	sz[u] = 1;
     int maxsub = 0; // max subtree size of u
 
-    for(int v:G[u])
-    {
-        if (v==pa)continue;
-        if (been_centroid[v])continue;
-        int c = tree_centroid(v, u, tree_size);
-		if(c != -1){
-			return c;
+	for(int v: G[u]){
+		if(been_centroid[v]) continue;
+        if (v==pa){
+			continue;
 		}
+         tree_centroid(v, u, tree_size, d+1);
         maxsub = max(maxsub, sz[v]);
         sz[u] += sz[v];
     }
     maxsub = max(maxsub, tree_size-sz[u]);
 
-    if (maxsub <= G.size()/2)
+    if (maxsub <= tree_size/2)
     {
-		return u;
+		gloc = u;
+	//	return u;
     }
-    return -1;
+//    return -1;
 }
 
-void dfs_depth_size(int u, int pa){
-	if(pa == -1){
-		depth[u] = 0;
+void dfs_count(int u, int pa, int d = 1){
+	if(k > d){
+		ans += cnt[k - d];
 	}
 	else{
-		depth[u] = depth[pa] + 1;
+		return;
 	}
-	sz[u] = 1;
 
+
+	for(auto it = G[u].begin(); it != G[u].end(); it++){
+		if(been_centroid[*it]) continue;
+
+		int v = *it;
+		if(v == pa) continue;
+
+		dfs_count(v, u, d+1);
+	}
+}
+
+void dfs_update(int u, int pa, int d = 1){
+	cnt[d]++;
+	sz[u] = 1;
 	for(int v: G[u]){
 		if(v == pa) continue;
-		if(been_centroid[v]) continue;
-		dfs_depth_size(v, u);
+		if(been_centroid[v]){
+			continue;
+		}	
+		dfs_update(v, u, d+1);
 		sz[u] += sz[v];
 	}
 }
 
-void dfs_count(int u, int pa, bool addin){
-	if(addin){
-		cnt[depth[u]]++;
-	}
-	else if(k > depth[u]){
-		ans += cnt[k-depth[u]];
-	}
-	for(int v: G[u]){
-		if(v == pa) continue;
-		if(been_centroid[v]) continue;
-		dfs_count(v, u, addin);
-	}
-}
-
 void solve(int subtree_u, int subtree_sz){ // subtree: maximum subtree containing u and all nodes haven't been centroid before.
-	if(subtree_sz == 1) return;
-	int K = min(subtree_sz, k);
-	int centroid = tree_centroid(subtree_u, -1, subtree_sz);
+	if(subtree_sz == 1 || subtree_sz < k) return;
+
+//	debug(subtree_u);
+//	debug(subtree_sz);
+	tree_centroid(subtree_u, -1, subtree_sz);
+	int centroid = gloc;
 	been_centroid[centroid] = true;
-	dfs_depth_size(centroid, -1);
-	memset(cnt, 0, sizeof(int) * (K + 1));
+
+	memset(cnt, 0, sizeof(int) * (k + 1));
 
 	for(int child: G[centroid]){
 		if(been_centroid[child]) continue;
-		dfs_count(child, -1, false);
-		dfs_count(child, -1, true);
+		dfs_count(child, -1);
+		dfs_update(child, -1);
 	}
+
+	ans += cnt[k];
 
 	for(int child: G[centroid]){
 		if(been_centroid[child]) continue;
 		solve(child, sz[child]);
 	}
-
 }
 
 int main(){
